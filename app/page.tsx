44374261fab8ba2014964ecb8b7de808ea0ec7a2
Mark Gillis <markjgillis@gmail.com>
2024-05-24 21:23:04 -0700

    Initial commit: skeleton has been created

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { getCategorisedArticles } from "@/lib/articles"
+
+const HomePage = () => {
+  const articles = getCategorisedArticles()
+
+  console.log(articles)
+  return (
+    <section className="mx-auto w-11/12 md:w-1/2 mt-20 flex flex-col gap-16 mb-20">
+      <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
+        <h1>minimal blog</h1>
+      </header>
+      <section className="md:grid md:grid-cols-2 flex flex-col gap-10"></section>
+    </section>
+  )
+}
+
+export default HomePage
